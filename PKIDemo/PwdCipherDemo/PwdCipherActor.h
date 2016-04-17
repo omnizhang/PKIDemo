35c0e43e76a3750f7cca12ef6fde77897b6ebf36
Ezfen <ezfen_zhang@163.com>
2016-04-17 19:24:00 +0800

    完成口令加解密功能

--- PKIDemo/PwdCipherDemo/PwdCipherActor.h
+++ PKIDemo/PwdCipherDemo/PwdCipherActor.h
@@ -7,7 +7,12 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "SymCipherActor.h"
 
-@interface PwdCipherActor : NSObject
+@interface PwdCipherActor : SymCipherActor
+
+- (NSData *)generateKeyWithPassword:(NSString *)password andSalt:(NSData *)salt andIterating:(int)numIterations;
+- (NSString *)DESEncrypt:(NSString *)plainText withPwdKey:(NSData *)pwdKey;
+- (NSString *)DESDecrypt:(NSString *)cipherText withPwdKey:(NSData *)pwdKey;
 
 @end
--- PKIDemo/PwdCipherDemo/PwdCipherActor.m
+++ PKIDemo/PwdCipherDemo/PwdCipherActor.m
@@ -7,25 +7,39 @@
 //
 
 #import "PwdCipherActor.h"
+#import "GTMBase64.h"
 
 @implementation PwdCipherActor
 
-- (NSData *)encryptPBEWithMD5AndDESData:(NSData *)inData password:(NSString *)password {
-    return nil;
-}
-
-
-
-
-
-
-- (NSData *)encodePBEWithMD5AndDESData:(NSData *)inData password:(NSString *)password direction:(int)direction {
-    static const char gSalt[] = {(unsigned char)0xaa, (unsigned char)0xd1, (unsigned char)0x3c, (unsigned char)0x31,
-                                 (unsigned char)0x53, (unsigned char)0xa2, (unsigned char)0xee, (unsigned char)0x05};
-    unsigned char *slat = (unsigned char *)gSalt;
-    int saltLen = strlen(gSalt);
-    int iterations = 15;
+- (NSData *)generateKeyWithPassword:(NSString *)password andSalt:(NSData *)salt andIterating:(int)numIterations {
+    unsigned char md5[CC_MD5_DIGEST_LENGTH];
+    memset(md5, 0, CC_MD5_DIGEST_LENGTH);
+    NSData* passwordData = [password dataUsingEncoding:NSUTF8StringEncoding];
     
-    return nil;
+    CC_MD5_CTX ctx;
+    CC_MD5_Init(&ctx);
+    CC_MD5_Update(&ctx, [passwordData bytes], [passwordData length]);
+    CC_MD5_Update(&ctx, [salt bytes], [salt length]);
+    CC_MD5_Final(md5, &ctx);
+    
+    for (int i=1; i<numIterations; i++) {
+        CC_MD5(md5, CC_MD5_DIGEST_LENGTH, md5);
+    }
+    
+    return [NSData dataWithBytes:md5 length:CC_MD5_DIGEST_LENGTH];
 }
+
+- (NSString *)DESEncrypt:(NSString *)plainText withPwdKey:(NSData *)pwdKey{
+    NSData *plainData = [plainText dataUsingEncoding:NSUTF8StringEncoding];
+    NSData *result = [self symCrypt:kCCEncrypt processData:plainData UsingAlgorithm:kCCAlgorithmDES key:pwdKey initalizationVector:nil options:kCCOptionPKCS7Padding | kCCOptionECBMode];
+    return [GTMBase64 stringByEncodingData:result];
+}
+
+- (NSString *)DESDecrypt:(NSString *)cipherText withPwdKey:(NSData *)pwdKey {
+    NSData *cipherData = [GTMBase64 decodeString:cipherText];
+    NSData *result = [self symCrypt:kCCDecrypt processData:cipherData UsingAlgorithm:kCCAlgorithmDES key:pwdKey
+                initalizationVector:nil options:kCCOptionPKCS7Padding | kCCOptionECBMode];
+    return [[NSString alloc] initWithData:result encoding:NSUTF8StringEncoding];
+}
+
 @end
