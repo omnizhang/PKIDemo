0fa4f33f9fabe3660c15dac763117127fb7501ca
Ezfen <ezfen_zhang@163.com>
2016-04-17 15:47:36 +0800

    修改摘要算法的输出格式为16进制

--- PKIDemo/Digest/DigestActor.m
+++ PKIDemo/Digest/DigestActor.m
@@ -16,21 +16,33 @@
     NSData *dataToDigest = [stringToDigest dataUsingEncoding:NSUTF8StringEncoding];
     UInt8 hash[CC_MD5_DIGEST_LENGTH];
     CC_MD5([dataToDigest bytes], (CC_LONG)[dataToDigest length], hash);
-    return [GTMBase64 stringByEncodingData:[NSData dataWithBytes:hash length:CC_MD5_DIGEST_LENGTH]];
+    NSMutableString *ret = [NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH];
+    for(int i = 0; i<CC_MD5_DIGEST_LENGTH; i++) {
+        [ret appendFormat:@"%02X",hash[i]];
+    }
+    return [ret copy];
 }
 
 - (NSString *)SHA1Digest:(NSString *)stringToDigest {
     NSData *dataToDigest = [stringToDigest dataUsingEncoding:NSUTF8StringEncoding];
     UInt8 hash[CC_SHA1_DIGEST_LENGTH];
     CC_SHA1([dataToDigest bytes], (CC_LONG)[dataToDigest length], hash);
-    return [GTMBase64 stringByEncodingData:[NSData dataWithBytes:hash length:CC_SHA1_DIGEST_LENGTH]];
+    NSMutableString *ret = [NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH];
+    for(int i = 0; i<CC_MD5_DIGEST_LENGTH; i++) {
+        [ret appendFormat:@"%02X",hash[i]];
+    }
+    return [ret copy];
 }
 
 - (NSString *)SHA256Digest:(NSString *)stringToDigest {
     NSData *dataToDigest = [stringToDigest dataUsingEncoding:NSUTF8StringEncoding];
     UInt8 hash[CC_SHA256_DIGEST_LENGTH];
     CC_SHA256([dataToDigest bytes], (CC_LONG)[dataToDigest length], hash);
-    return [GTMBase64 stringByEncodingData:[NSData dataWithBytes:hash length:CC_SHA256_DIGEST_LENGTH]];
+    NSMutableString *ret = [NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH];
+    for(int i = 0; i<CC_MD5_DIGEST_LENGTH; i++) {
+        [ret appendFormat:@"%02X",hash[i]];
+    }
+    return [ret copy];
 }
 
 @end
